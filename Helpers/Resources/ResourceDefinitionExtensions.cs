490a2aa0221998e9dccbff5a7130a975ecd407a7
Xceno <Xceno@protonmail.com>
2018-07-13 00:04:41 +0200

    feat(Helpers): Add helpers for working with Orchards ResourceManifests.

---
+++ Helpers/Resources/ResourceDefinitionExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace Orchard.Tools.Helpers.Resources {
+    using System;
+    using Orchard.UI.Resources;
+
+    public static class ResourceDefinitionExtensions {
+
+        /// <summary>
+        /// Adds the given url to the ResourceDefinition and appends a cacheBuster to it.
+        /// </summary>
+        /// <param name="self">The ResourceDefinition file to add the url to.</param>
+        /// <param name="url">The URL to add to the ResourceDefinition.</param>
+        /// <param name="debugUrl">The url to use in debug mode. Use this to specify an unminified, unobfuscated version of your resource.</param>
+        /// <param name="frequent">If true, the cacheBuster will be updated each minute. Otherwise it'll be changed daily.</param>
+        public static ResourceDefinition SetUrlWithCacheBuster(this ResourceDefinition self, string url, string debugUrl = null, bool frequent = false) {
+            if ( string.IsNullOrWhiteSpace(url) ) {
+                throw new ArgumentNullException("url");
+            }
+
+            var cacheBustQueryString = frequent
+                ? (long)new TimeSpan(DateTime.Now.Ticks - DateTime.MinValue.Ticks).TotalMinutes
+                : DateTime.Today.Ticks;
+
+            var urlWithCacheBuster = string.Format("{0}?{1}", url, cacheBustQueryString);
+
+            return !string.IsNullOrWhiteSpace(debugUrl)
+                ? self.SetUrl(urlWithCacheBuster, string.Format("{0}?{1}", debugUrl, cacheBustQueryString))
+                : self.SetUrl(urlWithCacheBuster);
+        }
+    }
+}
---
+++ Helpers/Resources/ResourceManifestExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Orchard.Tools.Helpers.Resources {
+    using Orchard.UI.Resources;
+
+    /// <summary>Provides extension methods for the <c>Orchard.UI.Resources.ResourceManifest</c></summary>
+    public static class ResourceManifestExtensions {
+        
+        /// <summary>
+        /// Adds a stylesheet to the resourceManifest.
+        /// The style sheets name will be converted to a minified and a normal resource url.
+        /// </summary>
+		public static ResourceDefinition DefineStyleWithAllUrls(this ResourceManifest manifest, string name, params string[] subfolders) {
+            var subfolderPath = string.Join("/", subfolders);
+            return manifest.DefineStyle(name).SetUrl(string.Format("{0}/{1}.min.css", subfolderPath, name), string.Format("{0}/{1}.css", subfolderPath, name));
+        }
+
+        /// <summary>
+        /// Adds a stylesheet to the resourceManifest.
+        /// The style sheets name will be converted to a minified and a normal resource url.
+        /// </summary>
+        public static ResourceDefinition DefineStyleWithAllUrls(this ResourceManifest manifest, string name) {
+            return manifest.DefineStyle(name).SetUrl(string.Format("{0}.min.css", name), string.Format("{0}.css", name));
+        }
+
+        /// <summary>
+        /// Adds a script to the resourceManifest.
+        /// The scripts name will be converted to a minified and a normal resource url.
+        /// </summary>
+        public static ResourceDefinition DefineScriptWithAllUrls(this ResourceManifest manifest, string name) {
+            return manifest.DefineScript(name).SetUrl(string.Format("{0}.min.js", name), string.Format("{0}.js", name));
+        }
+    }
+}
