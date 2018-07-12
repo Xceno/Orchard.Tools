0bfd435d1872ee44ed0ff1a738c61e0b6f188606
Xceno <Xceno@protonmail.com>
2018-07-12 23:29:04 +0200

    feat(Helpers): Add new helpers for working with dates and time.

---
+++ Helpers/Time/DateTimeOffsetExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Orchard.Tools.Helpers.Time {
+    using System;
+
+    public static class DateTimeOffsetExtensions {
+
+        /// <summary>
+        /// Adds the given number of seconds on top oft the UNIX Epoch (1970-01-01) and constructs a new DateTimeOffset accordingly.
+        /// </summary>
+        public static DateTimeOffset ToDateTimeOffsetFromUnixTimeSeconds(this long seconds) {
+            var unixEpoch = new DateTimeOffset(new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc));
+            unixEpoch = unixEpoch.AddSeconds(seconds);
+            return unixEpoch;
+        }
+
+        /// <summary>
+        /// Converts a DateTimeOffset to the number of milliseconds since the UNIX Epoch (1970-01-01).
+        /// </summary>
+        public static long ToUnixTimeSeconds(this DateTimeOffset dateTimeOffset) {
+            var unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var unixTimeStampInTicks = (dateTimeOffset.ToUniversalTime() - unixEpoch).Ticks;
+            return unixTimeStampInTicks / TimeSpan.TicksPerSecond;
+        }
+    }
+}
---
+++ Helpers/Time/TimeSpanExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Orchard.Tools.Helpers.Time {
+    using System;
+
+    public static class TimeSpanExtensions {
+        public static TimeSpan RoundTo(this TimeSpan timeSpan, int n) {
+            return TimeSpan.FromMinutes(n * Math.Ceiling(timeSpan.TotalMinutes / n));
+        }
+    }
+}
