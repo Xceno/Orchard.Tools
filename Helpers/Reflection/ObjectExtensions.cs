ac7f1ec66110bd3c0d73b456275a969e60819de9
Xceno <Xceno@protonmail.com>
2018-07-14 22:19:55 +0200

    feat(Helpers): Add more reflection utilities.
    ReflectFluidOn<T>: Allows to build a list of property names via reflection.
    This is useful for certain scenarios in a driver where you may want to make white- / blacklists of properties
    before running an update on a ContentItem.
    
    ObjectExtensions: Allows to a property name from an object. Supports enums.
    If you don't need enum-support, use Orchard.Utility.ReflectOn<T> instead.

---
+++ Helpers/Reflection/ObjectExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Orchard.Tools.Helpers.Reflection {
+    using System;
+    using System.Linq.Expressions;
+
+    [Obsolete("Please use orchards ReflectOn<T> extension methods unless you need to handle an enum. https://github.com/OrchardCMS/Orchard/issues/7411")]
+    public static class ObjectExtensions {
+        public static string PropertyName<T>(Expression<Func<T, object>> expression) {
+            var body = expression.Body as MemberExpression ?? ((UnaryExpression)expression.Body).Operand as MemberExpression;
+            return body.Member.Name;
+        }
+
+        public static string PropertyName<T>(this T self, Expression<Func<T, object>> expression) {
+            return PropertyName(expression);
+        }
+    }
+}
---
+++ Helpers/Reflection/ReflectFluidOn.cs
@@ -0,0 +1,46 @@
+﻿namespace Orchard.Tools.Helpers.Reflection {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using Orchard.Utility;
+
+    /// <summary>
+    /// A helper class to build a list of property names via reflection.
+    /// </summary>
+    /// <typeparam name="T">The type to reflect upon.</typeparam>
+    public class ReflectFluidOn<T> {
+
+        private readonly List<string> memberNames = new List<string>();
+
+        /// <summary>The property names currently added to the list.</summary>
+        public List<string> MemberNames {
+            get { return this.memberNames; }
+        }
+
+        public ReflectFluidOn<T> AddNameOf(Expression<Action<T>> expression) {
+            this.memberNames.Add(ReflectOn<T>.NameOf(expression));
+            return this;
+        }
+
+        public ReflectFluidOn<T> AddNameOf<TResult>(Expression<Func<T, TResult>> expression) {
+            this.memberNames.Add(ReflectOn<T>.NameOf(expression));
+            return this;
+        }
+
+        /// <summary>Removes all elements from the list.</summary>
+        public void Clear() {
+            this.memberNames.Clear();
+        }
+
+        /// <summary>
+        /// Returns the names of all public properties of the given type.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<string> GetAllPropertyNames() {
+            // No BindingFlags necessary. Instance, Public and Static are the defaults.
+            return typeof(T).GetProperties().Select(x => x.Name);
+        }
+    }
+}
