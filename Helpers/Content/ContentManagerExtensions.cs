191f9a6f6efd4700940eae5d8238680422887ed1
Xceno <Xceno@protonmail.com>
2018-07-14 22:21:26 +0200

    feat(Helpers): Add an extension that allows to get the contents of any IContainer.

---
+++ Helpers/Content/ContentManagerExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Orchard.Tools.Helpers.Content {
+    using System.Collections.Generic;
+    using Orchard.ContentManagement;
+    using Orchard.ContentManagement.Records;
+    using Orchard.Core.Common.Models;
+    using Orchard.Core.Containers.Models;
+
+    public static class ContentManagerExtensions {
+        /// <summary>
+        /// Queries the database for the given container and returns a list of the container contents with the specified Part.
+        /// </summary>
+        public static IEnumerable<TPart> GetContainerContentsOfType<TPart, TRecord>(this IContentManager contentManager, int containerId, VersionOptions versionOptions = null)
+            where TPart : ContentPart<TRecord>
+            where TRecord : ContentPartRecord {
+            return contentManager
+                .Query<TPart, TRecord>(versionOptions ?? VersionOptions.Published)
+                .Join<CommonPartRecord>()
+                .Where(x => x.Container != null && x.Container.Id == containerId)
+                .Join<ContainablePartRecord>()
+                .OrderByDescending(x => x.Position)
+                .List();
+        }
+    }
+}
