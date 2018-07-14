665444bef7c73889fdc152e7cab30e23f6c69846
Xceno <Xceno@protonmail.com>
2018-07-14 22:22:53 +0200

    feat(Helpers): A function that allows to write custom, type safe tokens based on some object.

---
+++ Helpers/Tokens/TokenExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Orchard.Tools.Helpers.Tokens {
+    using System;
+    using System.Linq.Expressions;
+    using Orchard.Localization;
+    using Orchard.Tokens;
+    using Orchard.Tools.Helpers.Reflection;
+
+    public static class TokenExtensions {
+        public static DescribeFor TokenFor<T>(this DescribeFor self, Expression<Func<T, object>> expression, LocalizedString name, LocalizedString description, string chainTarget = null) {
+#pragma warning disable CS0618 // We need enum support here
+            var property = ObjectExtensions.PropertyName(expression);
+#pragma warning restore CS0618 // Type or member is obsolete
+            return self.Token(property, name, description, chainTarget);
+        }
+    }
+}
